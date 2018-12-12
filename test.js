c43c9ca2235efe0926c8d97cae6f810ba8d9f6a4
Atte Huopainen <atte-mbp@Attes-MacBook-Pro-2.local>
2018-12-12 20:24:26 +0200

    First 3 CodeWars tasks

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+/*
+
 var materials = [
     'Hydrogen',
     'Helium',
@@ -53,3 +55,43 @@ function process_data_old_fashion(materials) {
 console.log(process_data(materials))
 console.log(process_data_alternative(materials))
 console.log(process_data_old_fashion(materials))
+
+//End of Villux examples
+//--------------------------------------------------------------------
+*/
+
+// Task: Find a needle and display index
+
+var haystack = ['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'];
+
+function findNeedle(){
+    return 'found the needle at position ' + haystack.indexOf('needle');
+}
+
+console.log(findNeedle(haystack))
+
+
+// Task: Get the number n (n>0) to return the reversed sequence from n to 1
+
+var number = 10
+
+for (n = number; n >= 1; n--)
+
+ console.log(n);
+ 
+// Task: Create a function that displays "Equal" or "Not Equal" for any value compared to 10
+
+var testNum = 10;
+
+function testNotEqual(val) {
+    if (val != testNum) { 
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  console.log(testNotEqual(10)
+  );
+
+  
+
